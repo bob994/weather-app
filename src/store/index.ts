b5ca84a83bac22e4f149e67bffb6806af02d3ab2
Bosko Nesic <bosko.nesic94@gmail.com>
2019-11-08 16:15:16 +0100

    Add redux dev tool

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,16 @@
 import { applyMiddleware, createStore, combineReducers } from 'redux';
 import { createLogicMiddleware } from 'redux-logic';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const logicMiddleware = createLogicMiddleware(); // TODO Add logics later
 
 const rootReducer = combineReducers({}); // TODO Add reducers later
 
-const store = createStore(rootReducer, applyMiddleware(logicMiddleware));
+const composeEnhancers = composeWithDevTools({});
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(logicMiddleware))
+);
 
 export { store };
